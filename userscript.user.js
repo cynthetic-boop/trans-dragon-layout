31421499c8250ecb4850984033350ccccbccba87
Cynth <cyntheticneutral@gmail.com>
2022-04-04 01:40:33 -0700

    new src

--- userscript.user.js
+++ userscript.user.js
@@ -12,7 +12,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://raw.githubusercontent.com/cynthetic-boop/neco-arc-overlay/main/place-neco-arc-overlay.png";
+            i.src = "https://raw.githubusercontent.com/cynthetic-boop/trans-dragon-layout/main/overlay.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
             console.log(i);
             return i;
